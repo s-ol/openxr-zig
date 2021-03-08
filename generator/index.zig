c4bf3120bfed2a8d53993e2e966617f4969edc55
s-ol <s-ol@users.noreply.github.com>
2021-03-08 11:43:12 +0100

    remove shader creation utilities

--- generator/index.zig
+++ generator/index.zig
@@ -1,7 +1,5 @@
 pub const generateXr = @import("openxr/generator.zig").generate;
 pub const XrGenerateStep = @import("openxr/build_integration.zig").GenerateStep;
-pub const generateSpirv = @import("spirv/generator.zig").generate;
-pub const ShaderCompileStep = @import("build_integration.zig").ShaderCompileStep;
 
 test "main" {
     _ = @import("xml.zig");
